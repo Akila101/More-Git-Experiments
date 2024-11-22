fa3fd3fc68502a3fe7e762ce2106624338dfa409
Akila101 <50293395+Akila101@users.noreply.github.com>
2024-11-22 22:33:21 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
 print("Python is Fun!!!")
+
+
+name = Akila
+
+input = input("Please type your name to confirm:", name)
+
+for i in input:
+  if i == name:
+    print("Hi,", input, ", hope you are well today!! Thanks for confirming your identity. You are good to proceed.")
+
+  else:
+    print("W A R N I N G!!! INVALID USER, CONTACTING NEAR BY AUTHORITIES. Security could be breached!!")
+
